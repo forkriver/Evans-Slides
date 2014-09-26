76547511d3412da4253cb4d7b1c4183f2f787daf
Patrick Johanneson <pjohanneson@gmail.com>
2014-09-26 18:22:12 -0500

    Init

---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Plugin Name: Evans Slide Tools
+ * Description: Tweaks to the Meteor Slides to work with the Evans Theatre website
+ * Author Name: Patrick Johanneson
+ * Author URI: http://patrickjohanneson.com/
+ */
+
+class Evans_Slide {
+
+	/**
+	 * Let's get everything started
+	 */
+	function __construct() {
+
+	}
+}
+
+new Evans_Slide();
